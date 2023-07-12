3392f04bd79d7a7c5d6ca63ff70f28e3b100445e
Kyle <kylemoore-@hotmail.com>
2023-07-12 14:39:06 -0400

    fixed some colours and margins.

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
       <div className="homePageHeader wrapper">
         {/* <h1>Lennox Drum Ltd.</h1> */}
         <img
-          src="./assets/Lennox-Drum-home.jpg"
+          src="./assets/Lennox-logo.jpg"
           alt="Lennox Drum ltd. logo"
         />
       </div>
