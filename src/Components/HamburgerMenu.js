de7cbd3294bc745edee8710acdce8d6ab7769a21
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2023-09-13 15:19:05 -0400

    changed hamburger functionality....still a work in progress

--- src/Components/HamburgerMenu.js
+++ src/Components/HamburgerMenu.js
@@ -1,56 +1,80 @@
-import React from "react";
-import { slide as Menu } from "react-burger-menu";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const hamburgerMenu = ({ hamburgerOpen, setHamburgerOpen }) => {
-  const closeMenu = (event) => {
-    event.preventDefault();
-    setHamburgerOpen(false);
+const HamburgerMenu = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <Menu isOpen={hamburgerOpen}>
-      <Link
-        id="home"
-        className="menu-item"
-        to="/"
-        aria-label="Navigate to home page"
-        title="Home"
-      >
-        Home
-      </Link>
-      <Link
-        id="productsAndServices"
-        className="menu-item"
-        to="/productsAndServices"
-        aria-label="Navigate to products and services page"
-        title="Products & Services"
-      >
-        Products & Services
-      </Link>
-      <Link
-        id="about"
-        className="menu-item"
-        to="/about"
-        aria-label="Navigate to about page"
-        title="About"
-      >
-        About
-      </Link>
-      <Link
-        id="contact"
-        className="menu-item"
-        to="/contact"
-        aria-label="Navigate to contact page"
-        title="Contact"
-      >
-        Contact
-      </Link>
-      <Link onClick={closeMenu} className="menu-item--small" href="null">
-        Settings
-      </Link>
-    </Menu>
+    <div className={`hamburger-menu ${menuOpen ? "open" : ""}`}>
+      <input
+        type="checkbox"
+        className="hamburger-checkbox"
+        id="hamburger-checkbox"
+        checked={menuOpen}
+        onChange={handleMenuToggle}
+      />
+      <label htmlFor="hamburger-checkbox" className="hamburger-label">
+        <span className="hamburger-icon"></span>
+      </label>
+      <div className="hamburger-content">
+        <ul>
+          <li>
+            <Link
+              id="home"
+              className="menu-item"
+              to="/"
+              aria-label="Navigate to home page"
+              title="Home"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              id="productsAndServices"
+              className="menu-item"
+              to="/productsAndServices"
+              aria-label="Navigate to products and services page"
+              title="Products & Services"
+            >
+              Products & Services
+            </Link>
+          </li>
+          <li>
+            <Link
+              id="about"
+              className="menu-item"
+              to="/about"
+              aria-label="Navigate to about page"
+              title="About"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              id="contact"
+              className="menu-item"
+              to="/contact"
+              aria-label="Navigate to contact page"
+              title="Contact"
+            >
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link className="menu-item--small" href="null">
+              Settings
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
-export default hamburgerMenu;
+export default HamburgerMenu;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,26 +1,16 @@
-import  { useState, useContext } from "react";
+import { useState, useContext } from "react";
 
 import { Link } from "react-router-dom";
 
 import HamburgerMenu from "./HamburgerMenu";
 import LanguageMenu from "./LanguageMenu";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { LanguageContext } from "./LanguageContext";
 
 const NavBar = () => {
   const { language } = useContext(LanguageContext);
-  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
   const [showProductsMenu, setShowProductsMenu] = useState(false);
-
-  const toggleHamburger = () => {
-    setHamburgerOpen(!hamburgerOpen);
-  };
-
-  const handleMouseEnter = () => {
-    setShowProductsMenu(true);
-  };
 
   const handleMouseLeave = () => {
     setShowProductsMenu(false);
@@ -48,31 +38,38 @@ const NavBar = () => {
   };
 
   return (
-    <nav>
-      <div className="navBar">
-        <img
-          src="./assets/Lennox-Logo.jpg"
-          alt="Lennox Drum logo"
-          className="logo"
-        />
-        <div className="navBarList">
-          <ul>
-            <li>
-              <Link
-                to="/"
-                aria-label="Navigate to home page"
-                title={translations[language].home}
-              >
-                {translations[language].home}
-              </Link>
-            </li>
-            <li>
-              <div className="dropdownTrigger" onClick={() => setShowProductsMenu(!showProductsMenu)} >
-                {translations[language].productsAndServices}
-                
+    <>
+      <nav>
+        <div className="navBar">
+          <HamburgerMenu />
+          <img
+            src="./assets/Lennox-Logo.jpg"
+            alt="Lennox Drum logo"
+            className="logo"
+          />
+          <div className="navBarList">
+            <ul>
+              <li>
+                <Link
+                  to="/"
+                  aria-label="Navigate to home page"
+                  title={translations[language].home}
+                >
+                  {translations[language].home}
+                </Link>
+              </li>
+              <li>
+                <div
+                  className="dropdownTrigger"
+                  onClick={() => setShowProductsMenu(!showProductsMenu)}
+                >
+                  {translations[language].productsAndServices}
+
                   <ul
                     onMouseLeave={handleMouseLeave}
-                    className= {`productsAndServicesDropdown ${showProductsMenu ? 'active' : 'inactive'}`}
+                    className={`productsAndServicesDropdown ${
+                      showProductsMenu ? "active" : "inactive"
+                    }`}
                   >
                     <li>
                       <Link
@@ -102,43 +99,33 @@ const NavBar = () => {
                       </Link>
                     </li>
                   </ul>
-                
-              </div>
-            </li>
-            <li>
-              <Link
-                to="/about"
-                aria-label={`Navigate to ${translations[language].about} page`}
-                title={translations[language].about}
-              >
-                {translations[language].about}
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/contact"
-                aria-label={`Navigate to ${translations[language].contact} page`}
-                title={translations[language].contact}
-              >
-                {translations[language].contact}
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="hamburger" onClick={toggleHamburger}>
-          <FontAwesomeIcon icon={faBars} className="hamburger-icon" />
-          {hamburgerOpen && (
-            <HamburgerMenu
-              hamburgerOpen={hamburgerOpen}
-              setHamburgerOpen={setHamburgerOpen}
-            />
-          )}
-        </div>
-        
+                </div>
+              </li>
+              <li>
+                <Link
+                  to="/about"
+                  aria-label={`Navigate to ${translations[language].about} page`}
+                  title={translations[language].about}
+                >
+                  {translations[language].about}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/contact"
+                  aria-label={`Navigate to ${translations[language].contact} page`}
+                  title={translations[language].contact}
+                >
+                  {translations[language].contact}
+                </Link>
+              </li>
+            </ul>
+          </div>
+
           <LanguageMenu />
-        
-      </div>
-    </nav>
+        </div>
+      </nav>
+    </>
   );
 };
 
